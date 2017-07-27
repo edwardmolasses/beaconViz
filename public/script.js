511ff30fbb2028209d8b66719d910a223bf6cd7c
Andrew Lachkovics <andrew.lachkovics@quickmobile.com>
2017-07-27 16:31:28 -0700

    added circular layout & sandbox force layout

--- public/script.js
+++ public/script.js
@@ -357,7 +357,7 @@ d3.tsv("data/whatwhere.tsv", function(error, data) {
                 o.color = colorByOcc(o.grp);
             } else {
                 o.color = "#cccccc";
-            }
+            }f
             // o.grp is 11-19 on the ordinal scale
             // x(o.grp) places us on the ordinal scale x-axis
             // o.x is the initial x position with a small randomization
